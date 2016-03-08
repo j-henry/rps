d90338776054b1f49b79b034a578298c150a7070
James <JHenry@Jamess-MacBook-Pro.local>
2016-03-08 16:31:27 -0500

    add rps round

--- rps.rb
+++ rps.rb
@@ -18,22 +18,26 @@ class Game
   attr_accessor :moves, :p1, :p2
 
   def initialize(p1=Player.new,p2=Computer.new)
-    self.moves = %w(rock paper scissors)
+    self.moves = %w(paper scissors rock)
     self.p1 = p1
     self.p2 = p2
   end
 
-  def possibel_moves
-
-  end
-
-  def get_move
-    gets
-  end
-
   def round
     p1.get_move
-    p2.get_move
+    p2.get_move(moves)
+  end
+
+  def compare(p1choice, p2choice)
+    # puts p1choice
+    # puts p2choice
+    if self.moves.index(p1choice) == self.moves.index(p2choice) - 2
+      "P1 wins!"
+    elsif self.moves.index(p1choice) == self.moves.index(p2choice) - 1
+      "P2 wins!"
+    else
+      "A draw!"
+    end
   end
 
 end
--- rps_test.rb
+++ rps_test.rb
@@ -36,10 +36,29 @@ class RpsTest < Minitest::Test
   end
 
   def test_round
+    skip
     x = Game.new
     assert x.round
-    assert x.p1.move(x.moves)
-    assert x.p2.move(x.moves)
+    assert x.p1.move
+    puts x.p1.move
+    assert x.p2.move
+    puts x.p2.move
   end
+
+  def test_scissors_rock
+    x = Game.new
+    assert x.compare("scissors", "rock") == "P2 wins!"
+  end
+
+  def test_scissors_paper
+    x = Game.new
+    assert x.compare("scissors", "paper") == "P1 wins!"
+  end
+
+  def test_scissors_scissors
+    x = Game.new
+    assert x.compare("scissors", "scissors") == "A draw!"
+  end
+
 
 end
